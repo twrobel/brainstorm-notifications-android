d32b75087b2f5391e15b5a3dc28b384255262cef
TomaszW <tomasz.wrobel@intelliware.ca>
2013-12-19 14:16:19 -0500

    cleanup

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Projects\android-workspace\BrainstormNotifications\bin\AndroidManifest.xml : \
-C:\Projects\android-workspace\BrainstormNotifications\AndroidManifest.xml \
--- bin/ServiceStartupActivity-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Projects\android-workspace\BrainstormNotifications\bin\ServiceStartupActivity-debug-unaligned.apk : \
-C:\Projects\android-workspace\BrainstormNotifications\bin\ServiceStartupActivity.ap_ \
-C:\Projects\android-workspace\BrainstormNotifications\bin\classes.dex \
-C:\Projects\android-workspace\BrainstormNotifications\libs\socket-io-client.jar \
-C:\Projects\android-workspace\BrainstormNotifications\libs\android-support-v4.jar \
